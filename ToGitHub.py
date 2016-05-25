91bbbd86b4580dd136d7562a25691c6b618b52c0
LouisBing <louisbinging@gmail.com>
2016-05-25 21:30:25 +0800

    6 com

--- ToGitHub.py
+++ ToGitHub.py
@@ -2,13 +2,3 @@
 
 print u'ToGitHub'
 print u'After first commit'
-
-print u'开始真正的编程吧'
-for i in range(1,10):
-    print u'%d^2 = %d' % (i, i*i)
-
-print u'为什么上次传的内容本次还上传'
-
-print u'开始真正的提交'
-for i in range(1,10):
-    print u'%d^2 = %d' % (i, i*i)
